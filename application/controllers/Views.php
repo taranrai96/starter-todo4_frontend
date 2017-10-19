ab8dd721f5e073144e79725d21b27e1642e8128f
taranrai96 <taranrai96@gmail.com>
2017-10-19 10:29:57 -0700

    closes #15, Completed Job 8

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -34,20 +34,35 @@ class Views extends Application
 		foreach ($undone as $task)
 			$converted[] = (array) $task;
 		$parms = ['display_tasks' => $converted];
+		$role = $this->session->userdata('userrole');
+		$parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
 		return $this->parser->parse('by_priority', $parms, true);
-
-		
-	
-		
 	}
-		function makeCategorizedPanel($tasks)
+	
+	function makeCategorizedPanel($tasks)
 	{
 		$parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
 		return $this->parser->parse('by_category', $parms, true);
 	}
 	
-
+	// complete flagged items
+	function complete() {
+		$role = $this->session->userdata('userrole');
+		if ($role != ROLE_OWNER) redirect('/views');
+		// loop over the post fields, looking for flagged tasks
+		foreach($this->input->post() as $key=>$value) {
+				if (substr($key,0,4) == 'task') {
+						// find the associated task
+						$taskid = substr($key,4);
+						$task = $this->tasks->get($taskid);
+						$task->status = 2; // complete
+						$this->tasks->update($task);
+				}
+		}
+		$this->index();
+	}
 }
+
 // return -1, 0, or 1 of $a's priority is higher, equal to, or lower than $b's
 	function orderByPriority($a, $b)
 	{
@@ -58,3 +73,4 @@ class Views extends Application
 		else
 			return 0;
 	}
+	
